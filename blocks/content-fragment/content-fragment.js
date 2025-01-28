238daf113f7b04080d4838a28a7854a5f5e9558e
Kevin Nennig <knennig213@gmail.com>
2025-01-28 17:34:54 -0500

    Create content-fragment.js

---
+++ blocks/content-fragment/content-fragment.js
@@ -0,0 +1,2 @@
+// https://github.com/markszulc/securbank-aem-ue/blob/f90c03ec2c7ec15945394173b8ba576d988f35d9/blocks/offer/offer.js
+// https://github.com/AdobeDevXSC/citisignal-one/blob/742c01c32672ef243883ca03fabecba8590854e9/blocks/content-fragment/content-fragment.js
