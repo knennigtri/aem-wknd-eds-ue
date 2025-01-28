d2db0718724b0f9fa02fe809067bb381d015e45f
Kevin Nennig <knennig213@gmail.com>
2025-01-28 18:20:17 -0500

    update

--- blocks/text-fields/text-fields.js
+++ blocks/text-fields/text-fields.js
@@ -2,8 +2,6 @@
 // https://github.com/adobe-experience-league/exlm/blob/0cb2cb5c622eb030c8f362a1646c19dc881803bd/blocks/teaser/teaser.js
 
 export default function decorate(block) {
-
-
   block.classList.add('new-class-name');
 
   // Find the div with data-aue-prop="style"
