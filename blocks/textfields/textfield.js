659d5da0dcc1e244158b89df214e127e5e670cd2
Kevin Nennig <knennig213@gmail.com>
2025-01-28 17:58:09 -0500

    Create textfield.js

---
+++ blocks/textfields/textfield.js
@@ -0,0 +1,6 @@
+// style example
+// https://github.com/adobe-experience-league/exlm/blob/0cb2cb5c622eb030c8f362a1646c19dc881803bd/blocks/teaser/teaser.js
+
+export default function decorate(block) {
+  block.classList.add('new-class-name');
+}
